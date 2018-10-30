ca9e907d7424522bbf0f8415e05daa1412d2941c
Beastlike3773 <manuel.gasser.ch@gmail.com>
2018-10-30 15:12:52 +0100

    [CHG] finished Shuffle exercise from topic06

---
+++ src/topic06/shuffle/Main.java
@@ -0,0 +1,42 @@
+package topic06.shuffle;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] before = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] after = new int[10];
+
+        after = shuffleArray(before);
+
+        for(int i = 0; i < before.length; i++){
+            System.out.println("Before: " + before[i] + ", After: " + after[i]);
+        }
+    }
+
+    public static int[] shuffleArray(int[] array) {
+        int[] tempArray = new int[array.length];
+        Arrays.fill(tempArray, Integer.MIN_VALUE);
+
+        Random rand = new Random();
+        boolean valueChanged = false;
+
+        for (int i = 0; i < array.length; i++) {
+            while (!valueChanged) {
+                int randInt = rand.nextInt(array.length);
+                if (tempArray[randInt] == Integer.MIN_VALUE) {
+                    tempArray[randInt] = array[i];
+                    valueChanged = true;
+                }
+            }
+
+            valueChanged = false;
+        }
+
+        return tempArray;
+    }
+}
+
